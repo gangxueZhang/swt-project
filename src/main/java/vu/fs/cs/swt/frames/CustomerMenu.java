34e64df2ce3b7fe60c25975982223a33015a1441
zloco <aploco1@etf.unsa.ba>
2016-04-23 22:03:22 +0200

    edited GUI

--- src/main/java/vu/fs/cs/swt/frames/CustomerMenu.java
+++ src/main/java/vu/fs/cs/swt/frames/CustomerMenu.java
@@ -12,6 +12,8 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EtchedBorder;
 
 public class CustomerMenu {
 
@@ -53,7 +55,7 @@ public class CustomerMenu {
 		//setting up the windows
 		JFrame frame = new JFrame("HelloWorld GUI");
 		frame.setLocation(new Point(100, 100));
-		frame.setMinimumSize(new Dimension(800, 500));
+		frame.setMinimumSize(new Dimension(700, 500));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -68,80 +70,177 @@ public class CustomerMenu {
 		lblWecheatem.setSize(new Dimension(50, 50));
 		
 		JPanel customerInformation = new JPanel();
-		customerInformation.setBounds(0, 40, 319, 149);
+		customerInformation.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		customerInformation.setBounds(10, 64, 399, 89);
 		frame.getContentPane().add(customerInformation);
 		customerInformation.setLayout(null);
 		
-		JLabel lblCustomerAccount = new JLabel("Customer Account no.:");
-		lblCustomerAccount.setBounds(27, 5, 172, 14);
+		JLabel lblCustomerAccount = new JLabel("Customer account no.:");
+		lblCustomerAccount.setBounds(10, 38, 172, 14);
 		lblCustomerAccount.setHorizontalTextPosition(SwingConstants.LEFT);
 		customerInformation.add(lblCustomerAccount);
 		
-		JLabel cAccountNumber = new JLabel("#12324353");
-		cAccountNumber.setBounds(209, 5, 100, 14);
-		cAccountNumber.setText("#002030");
+		JLabel cAccountNumber = new JLabel();
+		cAccountNumber.setBounds(192, 38, 100, 14);
+		if(_c != null) {
+			cAccountNumber.setText(_c.getAccountNumber());
+		}
 		customerInformation.add(cAccountNumber);
 		
-		JLabel lblSavingsAccountNo = new JLabel("Savings account no:");
-		lblSavingsAccountNo.setBounds(27, 24, 172, 14);
-		customerInformation.add(lblSavingsAccountNo);
-		
-		JLabel cSavingsNumber = new JLabel("#12342345");
-		cSavingsNumber.setBounds(209, 24, 100, 14);
-		customerInformation.add(cSavingsNumber);
-		
 		JLabel lblSavingsAccountBalance = new JLabel("Savings account balance:");
-		lblSavingsAccountBalance.setBounds(27, 49, 172, 14);
+		lblSavingsAccountBalance.setBounds(10, 63, 172, 14);
 		customerInformation.add(lblSavingsAccountBalance);
 		
-		JLabel cSavingsBalance = new JLabel("\u20AC 0.00");
-		cSavingsBalance.setBounds(209, 49, 46, 14);
+		JLabel cSavingsBalance = new JLabel();
+		cSavingsBalance.setBounds(192, 63, 46, 14);
+		if(_c != null) {
+			cSavingsBalance.setText("\u20AC" + _c.getSavingsAccount().getBalance());
+		}
 		customerInformation.add(cSavingsBalance);
 		
+		JLabel lblBasicInformation = new JLabel("Basic Information");
+		lblBasicInformation.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblBasicInformation.setBounds(10, 10, 144, 17);
+		customerInformation.add(lblBasicInformation);
+		
 		final JLabel lblCurrentMonth = new JLabel("APRIL 2016");
-		lblCurrentMonth.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblCurrentMonth.setFont(new Font("Tahoma", Font.BOLD, 14));
 		
 		lblCurrentMonth.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCurrentMonth.setOpaque(true);
 		lblCurrentMonth.setBackground(new Color(144, 238, 144));
-		lblCurrentMonth.setBounds(548, 51, 140, 101);
+		lblCurrentMonth.setBounds(419, 64, 255, 101);
 		frame.getContentPane().add(lblCurrentMonth);
 		
 		JPanel loansPanel = new JPanel();
-		loansPanel.setBounds(0, 189, 319, 161);
+		loansPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		loansPanel.setBounds(10, 153, 399, 282);
 		frame.getContentPane().add(loansPanel);
 		loansPanel.setLayout(null);
 		
-		JLabel lblLoans = new JLabel("Loans");
-		lblLoans.setBounds(10, 6, 50, 17);
-		lblLoans.setFont(new Font("Tahoma", Font.BOLD, 14));
-		loansPanel.add(lblLoans);
-		
-		JLabel lblLoan1 = new JLabel("1) Loan 1 Name");
-		lblLoan1.setBounds(70, 5, 100, 23);
-		lblLoan1.setText("1) Hi i'm a text");
-		loansPanel.add(lblLoan1);
-		
-		JLabel lblLoan2 = new JLabel("2) Loan 2 Name");
-		lblLoan2.setBounds(70, 29, 100, 14);
-		loansPanel.add(lblLoan2);
-		
-		JLabel lblLoan3 = new JLabel("3) Loan 3 Name");
-		lblLoan3.setBounds(70, 51, 100, 14);
-		loansPanel.add(lblLoan3);
-		
-		JButton btnAdvanceToNext = new JButton("Advance To Next Month");
-		btnAdvanceToNext.setBounds(39, 90, 211, 47);
-		btnAdvanceToNext.setBackground(new Color(237, 62, 103));
-		btnAdvanceToNext.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				lblCurrentMonth.setText("MAY 2016");
+		if(_c != null) {
+			if(!_c.getLoans().isEmpty()) {
+				JLabel lblLoans = new JLabel("Loans");
+				lblLoans.setBounds(10, 6, 50, 17);
+				lblLoans.setFont(new Font("Tahoma", Font.BOLD, 14));
+				loansPanel.add(lblLoans);
+				
+				JLabel lblLoan1 = new JLabel("1) Loan 1");
+				lblLoan1.setBounds(10, 43, 100, 23);
+				loansPanel.add(lblLoan1);
+				
+				JLabel lblLoan1Balance = new JLabel("a) Balance:");
+				lblLoan1Balance.setBounds(20, 63, 71, 23);
+				loansPanel.add(lblLoan1Balance);
+				
+				JLabel cLoan1Balance = new JLabel("");
+				cLoan1Balance.setBounds(94, 63, 71, 23);
+				loansPanel.add(cLoan1Balance);
+				
+				JLabel lblLoan1Interest = new JLabel("b) Interest:");
+				lblLoan1Interest.setBounds(20, 89, 71, 23);
+				loansPanel.add(lblLoan1Interest);
+				
+				JLabel cLoan1Interest = new JLabel("");
+				cLoan1Interest.setBounds(94, 89, 71, 23);
+				loansPanel.add(cLoan1Interest);
+				
+				JLabel lblLoan1MinPayment = new JLabel("c) Minimum payment:");
+				lblLoan1MinPayment.setBounds(185, 63, 123, 23);
+				loansPanel.add(lblLoan1MinPayment);
+				
+				JLabel cLoan1MinPayment = new JLabel("");
+				cLoan1MinPayment.setBounds(318, 63, 71, 23);
+				loansPanel.add(cLoan1MinPayment);
+				
+				JLabel lblLoan1Delinquent = new JLabel("d) Delinquent:");
+				lblLoan1Delinquent.setBounds(185, 89, 123, 23);
+				loansPanel.add(lblLoan1Delinquent);
+				
+				JLabel cLoan1Delinquent = new JLabel("");
+				cLoan1Delinquent.setBounds(318, 89, 71, 23);
+				loansPanel.add(cLoan1Delinquent);
+				
+				if(_c.getLoans().size() > 1) {
+					JLabel lblLoan2Interest = new JLabel("b) Interest:");
+					lblLoan2Interest.setBounds(20, 166, 71, 23);
+					loansPanel.add(lblLoan2Interest);
+					
+					JLabel lblLoan2Delinquent = new JLabel("d) Delinquent:");
+					lblLoan2Delinquent.setBounds(185, 167, 123, 23);
+					loansPanel.add(lblLoan2Delinquent);
+					
+					JLabel cLoan2MinPayment = new JLabel("");
+					cLoan2MinPayment.setBounds(318, 140, 71, 23);
+					loansPanel.add(cLoan2MinPayment);
+					
+					JLabel cLoan2Delinquent = new JLabel("");
+					cLoan2Delinquent.setBounds(318, 166, 71, 23);
+					loansPanel.add(cLoan2Delinquent);
+					
+					JLabel lblLoan2 = new JLabel("2) Loan 2");
+					lblLoan2.setBounds(10, 120, 100, 14);
+					loansPanel.add(lblLoan2);
+					
+					JLabel lblLoan2Balance = new JLabel("a) Balance:");
+					lblLoan2Balance.setBounds(20, 140, 71, 23);
+					loansPanel.add(lblLoan2Balance);
+					
+					JLabel lblLoan2MinPayment = new JLabel("c) Minimum payment:");
+					lblLoan2MinPayment.setBounds(185, 140, 123, 23);
+					loansPanel.add(lblLoan2MinPayment);
+					
+					JLabel cLoan2Balance = new JLabel("");
+					cLoan2Balance.setBounds(94, 140, 71, 23);
+					loansPanel.add(cLoan2Balance);
+					
+					JLabel cLoan2Interest = new JLabel("");
+					cLoan2Interest.setBounds(101, 166, 71, 23);
+					loansPanel.add(cLoan2Interest);
+					
+					if(_c.getLoans().size() > 2) {
+						JLabel cLoan3Balance = new JLabel("");
+						cLoan3Balance.setBounds(94, 221, 71, 23);
+						loansPanel.add(cLoan3Balance);
+						
+						JLabel cLoan3Interest = new JLabel("");
+						cLoan3Interest.setBounds(94, 247, 71, 23);
+						loansPanel.add(cLoan3Interest);
+						
+						JLabel lblLoan3 = new JLabel("3) Loan 3");
+						lblLoan3.setBounds(10, 201, 100, 14);
+						loansPanel.add(lblLoan3);
+						
+						JLabel lblLoan3Balance = new JLabel("a) Balance:");
+						lblLoan3Balance.setBounds(20, 221, 71, 23);
+						loansPanel.add(lblLoan3Balance);
+						
+						JLabel lblLoan3Interest = new JLabel("b) Interest:");
+						lblLoan3Interest.setBounds(20, 247, 71, 23);
+						loansPanel.add(lblLoan3Interest);
+						
+						JLabel lblLoan3MinPayment = new JLabel("c) Minimum payment:");
+						lblLoan3MinPayment.setBounds(185, 221, 123, 23);
+						loansPanel.add(lblLoan3MinPayment);
+						
+						JLabel lblLoan3Delinquent = new JLabel("d) Delinquent:");
+						lblLoan3Delinquent.setBounds(185, 248, 123, 23);
+						loansPanel.add(lblLoan3Delinquent);
+						
+						JLabel cLoan3MinPayment = new JLabel("");
+						cLoan3MinPayment.setBounds(318, 221, 71, 23);
+						loansPanel.add(cLoan3MinPayment);
+						
+						JLabel cLoan3Delinquent = new JLabel("");
+						cLoan3Delinquent.setBounds(318, 247, 71, 23);
+						loansPanel.add(cLoan3Delinquent);
+					}
+				}
 			}
-		});
-		loansPanel.add(btnAdvanceToNext);
+		}
 		
 		JPanel optionsPanel = new JPanel();
-		optionsPanel.setBounds(498, 238, 255, 178);
+		optionsPanel.setBounds(419, 257, 255, 178);
 		frame.getContentPane().add(optionsPanel);
 		optionsPanel.setLayout(new GridLayout(2, 2, 4, 4));
 		
@@ -174,6 +273,16 @@ public class CustomerMenu {
 		optionsPanel.add(btnMakePayment);
 		optionsPanel.add(btnWithdrawSavings);
 		optionsPanel.add(btnDepositToSavings);
+		
+		JButton btnAdvanceToNext = new JButton("Advance To Next Month");
+		btnAdvanceToNext.setBounds(419, 176, 255, 47);
+		frame.getContentPane().add(btnAdvanceToNext);
+		btnAdvanceToNext.setBackground(new Color(237, 62, 103));
+		btnAdvanceToNext.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				lblCurrentMonth.setText("MAY 2016");
+			}
+		});
 		
 		//finally, displaying the window
 		frame.pack();
